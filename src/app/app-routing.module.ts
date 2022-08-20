cbbc32ab340923417fbead69403bc69aa84c6fd8
shubh <shubhvjain@gmail.com>
2022-08-20 10:02:46 +0200

    added home page component and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [{ path: 'core', loadChildren: () => import('./core/core.module').then(m => m.CoreModule) }, { path: 'plugin', loadChildren: () => import('./plugins/plugins.module').then(m => m.PluginsModule) }, { path: 'setting', loadChildren: () => import('./settings/settings.module').then(m => m.SettingsModule) }];
+import { HomeComponent } from './home/home.component';
+const routes: Routes = [
+  { path: '', component:HomeComponent },
+  { path: 'core', loadChildren: () => import('./core/core.module').then(m => m.CoreModule) }, 
+  { path: 'plugin', loadChildren: () => import('./plugins/plugins.module').then(m => m.PluginsModule) }, 
+  { path: 'setting', loadChildren: () => import('./settings/settings.module').then(m => m.SettingsModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
